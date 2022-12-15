7a9f59c972ab8a862c63bce56736d73dbbba6b23
Damian Choptiany <damian.choptiany@gmail.com>
2022-12-15 13:03:09 +0100

    Added client server sockets

--- src/main/java/Controller/ClientLauncher.java
+++ src/main/java/Controller/ClientLauncher.java
@@ -1,25 +1,44 @@
 package Controller;
 
-import View.BoardGui;
 import View.ClientView;
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.geometry.Pos;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
-import javafx.stage.WindowEvent;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
 
 public class ClientLauncher extends Application {
     Stage stage;
+    Socket socket;
+    PrintWriter out;
+    BufferedReader in;
+
     @Override
     public void start(Stage stage) {
         stage = new ClientView();
+
+        try {
+            socket = new Socket("localhost", 4444);
+            out = new PrintWriter(socket.getOutputStream(), true);
+            in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        }
+        catch(Exception e) {
+            e.printStackTrace();
+        }
     }
+
+    @Override
+    public void stop() {
+        try {
+            socket.close();
+        }
+        catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+
     public static void main(String[] args) {
         launch(args);
     }
--- src/main/java/Controller/ServerLauncher.java
+++ src/main/java/Controller/ServerLauncher.java
@@ -1,17 +1,11 @@
 package Controller;
 
-import View.BoardGui;
 import View.ServerView;
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.geometry.Pos;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
+
+import java.net.ServerSocket;
+import java.net.Socket;
 
 public class ServerLauncher extends Application {
     Stage stage;
@@ -19,6 +13,18 @@ public class ServerLauncher extends Application {
     @Override
     public void start(Stage stage) {
         stage = new ServerView();
+
+        try (ServerSocket serverSocket = new ServerSocket(4444)) {
+            System.out.println("Server nasluchuje na porcie 4444");
+
+            while(true) {
+                Socket socket = serverSocket.accept();
+                System.out.println("Polaczyl sie nowy klient.");
+            }
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public static void main(String[] args) {
