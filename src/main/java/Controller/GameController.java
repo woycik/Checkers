b1df3ef0c4bcebe324651d91c9186999192e6233
Damian Choptiany <damian.choptiany@gmail.com>
2022-12-16 14:12:41 +0100

    Removed checkers

--- src/main/java/Controller/GameController.java
+++ src/main/java/Controller/GameController.java
@@ -1,14 +1,15 @@
 package Controller;
 
-import Model.Checkers;
-import Model.Pawn;
+import Model.Board;
+import Model.PlayerTurn;
 
 public abstract class GameController {
-    Checkers checkers;
+    public Board board;
+    public PlayerTurn playerTurn;
     public abstract boolean play(int x, int y,int i, int j,String color);
     public abstract boolean isMoveLegal(int i, int j, int m, int n);
     public abstract int getPawnRows();
     public int getBoardSize() {
-        return checkers.board.size;
+        return board.size;
     }
 }
--- src/main/java/Model/Checkers.java
+++
@@ -1,6 +0,0 @@
-package Model;
-
-public abstract class Checkers {
-    public Board board;
-    public PlayerTurn playerTurn;
-}
